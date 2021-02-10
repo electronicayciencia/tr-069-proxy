7b528adaa0421d8609e2f79ca907f1410a1a55f0
Electronica y ciencia <electronicayciencia@gmail.com>
2021-02-10 11:41:00 +0100

    Better error management

--- part2/README.md
+++ part2/README.md
@@ -1,15 +1,41 @@
 # Sagemcom F@ST 5657 configuration decryptor
 
-Based on NoConroy's posts
+
+
+## Prerequisites
+
+This scripts runs in Python3 environment.
+
+Yoy will need a Crypto.AES module, provided by pycryptodome, pycrypto, cryptodomex  or whatever Crypto library you like.
+
+    pip install pycryptodome
+
+If you are using Windows, your Crypto library may be not recognized until you remane its directory from `crypto` to `Crypto` (uppercase). Read [this](https://github.com/pycrypto/pycrypto/issues/156).
+
+Or you may prefer to use compiled binary from `dist` directory.
+
+## Showconfig usage
+
+Decrypts Sagemcom F@ST 5657 configuration file and display relevant fields.
+
+    showconfig.py infile [outfile]
+
+- infile: Encrypted configuration file like `device.cfg`
+- outfile: Output XML file if you want to save it.
+
+The file `showconfig.json` must be in the same directory.
+
+## gsdf usage
+
+This is a raw encrypt/decrypt routine I copied from NoConroy's posts
 [Sagemcom F@ST5355 Reverse Engineering - Part 2](https://web.archive.org/web/20180129221204/https://noconroy.net/sagemcom-fast5355-re-p2.html)
 and
 [Sagemcom F@ST5355 Reverse Engineering - Part 3](https://web.archive.org/web/20180129221204/https://noconroy.net/sagemcom-fast5355-re-p3.html).
 
-## Usage
+The only changes I made are:
 
-Python3 needed.
-
-    pip install pycryptodome
+- updated key
+- ported to python3
 
 To decrypt:
 
@@ -18,3 +44,8 @@ To decrypt:
 To encrypt:
 
     gzip -nc device.xml | python gsdf.py e > device.cfg
+
+
+## Create windows executable
+
+    pyinstaller.exe --onefile .\showconfig.py
--- part2/showconfig.py
+++ part2/showconfig.py
@@ -10,6 +10,38 @@ import json
 import argparse
 
 
+def loadconfig(file):
+    ''' Read configuration file. Must be json. 
+    Return dict or terminate program. '''
+    try:
+        with open(file) as configfile_h:
+            config = json.load(configfile_h)
+    except:
+        print("Cannot read configuration file %s or it is damaged." % file)
+        sys.exit(1)
+
+    return config
+
+
+def parse_cmdline():
+    ''' No parameters. Return Parser object. Exit and display help on error.'''
+    parser = argparse.ArgumentParser(
+        description='Decrypts Sagemcom F@ST 5657 configuration file '
+        ' and display relevant fields.')
+
+    parser.add_argument('infile', help='Encrypted configuration file')
+    parser.add_argument('outfile', nargs='?',
+                        help='Output XML file (optional)')
+
+    if len(sys.argv) == 1:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+
+    args = parser.parse_args()
+
+    return args
+
+
 def show_interesting_fields(xml, paths):
     tree = ET.ElementTree(ET.fromstring(xml))
     root = tree.getroot()
@@ -26,24 +58,21 @@ def show_interesting_fields(xml, paths):
 
 def main():
     configfile = "showconfig.json"
+    args = parse_cmdline()
+    config = loadconfig(configfile)
 
-    parser = argparse.ArgumentParser(
-        description='Decrypts Sagemcom F@ST 5657 configuration file.')
+    try:
+        ciphertext = open(args.infile, "rb").read()
+    except:
+        print("Cannot read %s." % args.infile)
+        sys.exit(1)
 
-    parser.add_argument('infile', help='Encrypted configuration file')
-    parser.add_argument('outfile', nargs='?', help='Output XML file')
-    args = parser.parse_args()
-
-    with open(configfile) as configfile_h:
-        config = json.load(configfile_h)
-
-    if config is None:
-        print("Cannot load config from %s" % configfile)
-        return
-
-    ciphertext = open(args.infile, "rb").read()
-    xml_gz = decrypt(ciphertext)
-    xml = gzip.decompress(xml_gz)
+    try:
+        xml_gz = decrypt(ciphertext)
+        xml = gzip.decompress(xml_gz)
+    except:
+        print("Decryption failed. Invalid router backup file.")
+        sys.exit(1)
 
     show_interesting_fields(xml, config['paths'])
 
